7cf435ca550ab6095b8e5c57002e93e3d1c3e077
sujeet <suryawanshisujeet@gmail.com>
2024-09-06 20:51:37 +0530

    init

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,34 @@
-import Image from "next/image";
 
-export default function Home() {
+export default async function Page() {
+  var dataString = 'eyJhbGciOiJIUzI1NiIsImNsaWVudGlkIjoidWZvc3NnZW52MiJ9.ewoibWVyY2lkIjoiVUZPU1NHRU5WMiIsCiJvcmRlcmlkIjoiVUFUTTR4Y2RmcmRyZmRkIiwKImFtb3VudCI6IjEwMC4wMCIsCiJvcmRlcl9kYXRlIjoiMjAyNC0wMi0yMFQxMDoxMjowMCswNTozMCIsCiJjdXJyZW5jeSI6IjM1NiIsCiJydSI6Imh0dHBzOi8vd3d3Lm1lcmNoYW50LmNvbS8iLAoiYWRkaXRpb25hbF9pbmZvIjp7CiAgImFkZGl0aW9uYWxfaW5mbzEiOiJUZXN0IiwKICAiYWRkaXRpb25hbF9pbmZvMiI6IlRlc3QxIgogIH0sCiJpdGVtY29kZSI6IkRJUkVDVCIsCiJkZXZpY2UiOnsKICAiaW5pdF9jaGFubmVsIjoiaW50ZXJuZXQiLAogICJpcCI6Ijc2Ljc2LjIxLjIxIiwKICAidXNlcl9hZ2VudCI6Ik1vemlsbGEvNS4wKFdpbmRvd3NOVDEwLjA7V09XNjQ7cnY6NTEuMClHZWNrby8yMDEwMDEwMUZpcmVmb3gvNTEuMCIsCiAgImFjY2VwdF9oZWFkZXIiOiJ0ZXh0L2h0bWwiLAogICJicm93c2VyX3R6IjoiLTMzMCIsCiAgImJyb3dzZXJfY29sb3JfZGVwdGgiOiIzMiIsCiAgImJyb3dzZXJfamF2YV9lbmFibGVkIjoiZmFsc2UiLAogICJicm93c2VyX3NjcmVlbl9oZWlnaHQiOiI2MDEiLAogICJicm93c2VyX3NjcmVlbl93aWR0aCI6IjY1NyIsCiAgImJyb3dzZXJfbGFuZ3VhZ2UiOiJlbi1VUyIsCiAgImJyb3dzZXJfamF2YXNjcmlwdF9lbmFibGVkIjoidHJ1ZSIKICB9Cn0.TCSShBWnGC9_NzZ1xgDJDP9DUdJzN2qNLoyzBNoGIN8';
+
+  const options = {
+    method: 'POST',
+    body: dataString,
+    headers: { 
+      'Content-Type': 'application/jose',
+      'Accept': 'application/jose',
+      'BD-Traceid':'1723817310ABCD',
+      'BD-Timestamp':'1723817310'
+  }
+}
+
+const url = 'https://uat1.billdesk.com/u2/payments/ve1_2/orders/create';
+let jsonResponse
+    try {
+      const response = await fetch(url, options)
+      jsonResponse= await response.text();
+      
+      console.log('JSON response', jsonResponse);
+    } catch(err) {
+      console.log('ERROR', err);
+    }
+
+
+  
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+    <ul>
+      {jsonResponse}
+    </ul>
+  )
 }
